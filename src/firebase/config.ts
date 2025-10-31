f428aa7dbe29f45eb2872d3c99d96e4a1090f91b
Luis Eduardo Paim Longhi <luis.longhi@undb.edu.br>
2025-10-31 19:07:20 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3759592126-ec313",
+  "appId": "1:489965222377:web:b28ab75830604943e1b378",
+  "apiKey": "AIzaSyBHRnGcq9h2I1V4F8a6ROEvMkRzUKd0NyI",
+  "authDomain": "studio-3759592126-ec313.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "489965222377"
+};
