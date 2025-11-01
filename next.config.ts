0df855ea529e7b527cb3652d5122c32b7358669f
Luis Eduardo Paim Longhi <luis.longhi@undb.edu.br>
2025-11-01 18:45:57 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -12,7 +12,7 @@ const nextConfig: NextConfig = {
       { protocol: 'https', hostname: 'images.unsplash.com', pathname: '/**' },
       { protocol: 'https', hostname: 'picsum.photos', pathname: '/**' },
       // Host que realmente serve o arquivo do Storage
-      { protocol: 'https-alias', hostname: 'firebasestorage.googleapis.com', pathname: '/**' },
+      { protocol: 'https', hostname: 'firebasestorage.googleapis.com', pathname: '/**' },
       // (Opcional) Se em algum momento usar link direto regional
       { protocol: 'https', hostname: 'studio-3759592126-ec313.firebasestorage.app', pathname: '/**' },
     ],
