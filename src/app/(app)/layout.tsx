559f091bd67bb663cef8920bbe9b10ffb642a360
Luis Eduardo Paim Longhi <luis.longhi@undb.edu.br>
2025-11-02 15:37:03 +0000

    Try fixing this error: `Runtime Error: Error: Cannot find the middleware

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from '@/components/theme-provider';
 import { BottomNav } from '@/components/layout/bottom-nav';
 import { Header } from '@/components/layout/header';
 import { FirebaseClientProvider } from '@/firebase/client-provider';
+import { AuthGuard } from '@/components/layout/auth-guard';
 
 export default function AppLayout({
   children,
@@ -12,11 +13,13 @@ export default function AppLayout({
   return (
     <FirebaseClientProvider>
       <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
-        <div className="flex min-h-screen w-full flex-col">
-          <Header />
-          <main className="flex-1 p-4 pb-20 sm:p-6">{children}</main>
-          <BottomNav />
-        </div>
+        <AuthGuard>
+          <div className="flex min-h-screen w-full flex-col">
+            <Header />
+            <main className="flex-1 p-4 pb-20 sm:p-6">{children}</main>
+            <BottomNav />
+          </div>
+        </AuthGuard>
       </ThemeProvider>
     </FirebaseClientProvider>
   );
